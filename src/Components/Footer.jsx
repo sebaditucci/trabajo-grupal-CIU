4e99dac730faaa0f4f56037979d5b6c372220427
rodrisebok <rodrigosebok@gmail.com>
2024-11-27 18:45:18 -0300

    detalles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,19 +6,19 @@ export default function Footer() {
     <div className = {styles.footer}>
         <div className = {styles.datos}>
             <p className = {styles.nombre}>Sebastián Di Tucci</p>
-            <a href = "sebastiandit240@gmail.com" target = "_blank" className = {styles.link}>sebastiandit240@gmail.com</a>
+            <a href = "mailto:sebastiandit240@gmail.com" title='Link a Mail' target = "_blank" className = {styles.link}>sebastiandit240@gmail.com</a>
         </div>
         <div className = {styles.datos}>
             <p className = {styles.nombre}>Rodrigo Sebok</p>
-            <a href = "rodrigosebok@gmail.com" target = "_blank" className = {styles.link}>rodrigosebok@gmail.com</a>
+            <a href = "mailto:rodrigosebok@gmail.com" title='Link a Mail' target = "_blank" className = {styles.link}>rodrigosebok@gmail.com</a>
         </div>
         <div className = {styles.datos}>
             <p className = {styles.nombre}>Magalí Seoane</p>
-            <a href = "magalielizabeth.seoane@estudiantes.unahur.edu.ar" target = "_blank" className = {styles.link}>magalielizabeth.seoane@estudiantes.unahur.edu.ar</a>
+            <a href = "mailto:magalielizabeth.seoane@estudiantes.unahur.edu.ar" title='Link a Mail' target = "_blank" className = {styles.link}>magalielizabeth.seoane@estudiantes.unahur.edu.ar</a>
         </div>
         <div className = {styles.datos}>
           <p className = {styles.nombre}>Franco Trautweiler</p>
-          <a href = "francotraut9@gmail.com" target = "_blank" className = {styles.link}>francotraut9@gmail.com</a>
+          <a href = "mailto:francotraut9@gmail.com" title='Link a Mail' target = "_blank" className = {styles.link}>francotraut9@gmail.com</a>
         </div>
     </div>
   )
